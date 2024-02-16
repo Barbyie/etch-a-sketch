0eb6f0b8538bf3980bf9f711959627b03f2b67c4
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-16 11:23:48 -0300

    Create big container and test adding smaller squares inside

---
+++ script.js
@@ -0,0 +1,15 @@
+const row= 16;
+const column= 16;
+const height = '500px';
+const width = '500px';
+
+let outerContainer = document.querySelector('.container');
+outerContainer.style.width = width;
+outerContainer.style.height = height;
+
+for (let i = 0; i <= (row * column); i++) {
+  let innerContainer = document.createElement('div');
+  innerContainer.style.height = '15px';
+  innerContainer.style.width = '15px';
+  outerContainer.appendChild(innerContainer);
+};
