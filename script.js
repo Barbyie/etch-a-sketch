a3efe450bf0442ea7f270b2dd6e93a07800e21b4
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-17 13:37:09 -0300

    Fix size of the divs

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const row= 16;
 const column= 16;
-const height = '500px';
-const width = '500px';
+const height = 500;
+const width = 500;
+  
 
 let outerContainer = document.querySelector('.container');
-outerContainer.style.width = width;
-outerContainer.style.height = height;
+outerContainer.style.width = `${width}px`;
+outerContainer.style.height = `${height}px`;
 
-for (let i = 0; i <= (row * column); i++) {
+for (let i = 1; i <= (row * column); i++) {
   let innerContainer = document.createElement('div');
-  innerContainer.style.height = '15px';
-  innerContainer.style.width = '15px';
-  innerContainer.style.flexGrow = '1';
+  innerContainer.style.height = `${height / row}px`;
+  innerContainer.style.width = `${width / column}px`;
+  innerContainer.style.flexWrap = 'wrap';
   outerContainer.appendChild(innerContainer);
+  console.log(i);
 };
