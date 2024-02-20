7447815e069d83d32862a26cda762dcfdb674107
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-19 22:00:02 -0300

    Add color to grid created using the button

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-const row= 16;
-const column= 16;
 const height = 600;
 const width = 600;
   
 
 let button = document.querySelector('.promptButton');
-let condition = true;
-let size = 0;
+button.addEventListener('click', () => {
+  if (gridSize() > 100 || gridSize() <= 0) {
+    alert("Invalid input, please type a size between 1 and 100");
+  } else {
+    createGrid(gridSize());
+    console.log("buttonclick");
+    colorGrid();
+  }; 
+});
 
-// button.addEventListener('click', () => {
-//   while(condition) {
-//     size = Number(prompt("Set up size of the grid"));
-//     console.log(size);
-//     if (size > 100 || size <= 0) {
-//       alert("Invalid input, please type a size between 1 and 100");
-//     } else {
-//       condition = false;
-//     }; 
-//   };
-// });
-
-let outerContainer = document.querySelector('.container');
-outerContainer.style.width = `${width}px`;
-outerContainer.style.height = `${height}px`;
-
-for (let i = 1; i <= (size * size); i++) {
-  let innerContainer = document.createElement('div');
-  innerContainer.classList.add('insideDiv');
-  innerContainer.style.height = `${height / size}px`;
-  innerContainer.style.width = `${width / size}px`;
-  innerContainer.style.flexWrap = 'wrap';
-  outerContainer.appendChild(innerContainer);
-  console.log(i);
+function gridSize () {
+  let size = Number(prompt("What will be the size of the grid ?"));
+  console.log("gridSize function");
+  return size;
 };
 
-let changeColor = document.querySelectorAll('.insideDiv');
-for (let i = 0 ; i < changeColor.length; i++) {
-  changeColor[i].addEventListener('mouseover', () => {
-    changeColor[i].style.backgroundColor = 'black';
-  });
+function createGrid (size) {
+  let outerContainer = document.querySelector('.container');
+  outerContainer.style.width = `${width}px`;
+  outerContainer.style.height = `${height}px`;
+
+  for (let i = 1; i <= (size * size); i++) {
+    let innerContainer = document.createElement('div');
+    innerContainer.classList.add('insideDiv');
+    innerContainer.style.height = `${height / size}px`;
+    innerContainer.style.width = `${width / size}px`;
+    innerContainer.style.flexWrap = 'wrap';
+    outerContainer.appendChild(innerContainer);
+  };
+
+};
+
+function colorGrid () {
+  let changeColor = document.querySelectorAll('.insideDiv');
+  for (let i = 0 ; i < changeColor.length; i++) {
+    changeColor[i].addEventListener('mouseover', () => {
+      changeColor[i].style.backgroundColor = 'black';
+    });
+  };
 };
